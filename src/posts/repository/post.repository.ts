81fc7f8d1318ee6f87d624c2e632187f110a0b5d
Felipe Silveira <felipe@atalhus.com.br>
2025-06-26 13:19:24 -0300

    mudando types

--- src/posts/repository/post.repository.ts
+++ src/posts/repository/post.repository.ts
@@ -1,9 +1,9 @@
 import { UpdatePostDto } from '../dto/update-post.dto';
 import { IPost } from '../schemas/models/posts.interface';
-import { Post } from '../schemas/posts.schema';
+import { CreatePostDto } from '../dto/create-post.dto';
 
 export abstract class PostRepository {
-  abstract create(post: any): Promise<Post>;
+  abstract create(post: CreatePostDto): Promise<IPost>;
   abstract findAllPublished(): Promise<IPost[]>; //For students
   abstract findAll(limit: number, page: number): Promise<IPost[]>; //For Teachers
   abstract findOne(id: string): Promise<IPost | null>;
--- src/posts/schemas/models/posts.interface.ts
+++ src/posts/schemas/models/posts.interface.ts
@@ -1,4 +1,5 @@
 export interface IPost {
+  id: string;
   title: string;
   description: string;
   image: string;
